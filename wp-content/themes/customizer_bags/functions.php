3a3e1194b0856da4502ef4304c26b4b283930092
jose.lazo <jose.lazo@sngular.team>
2018-09-17 16:20:54 +0200

    Childtheme to Genesis framework

---
+++ wp-content/themes/customizer_bags/functions.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once( get_template_directory() . '/lib/init.php' );
+
+define( 'CHILD_THEME_NAME', 'customizer_bags');
+define( 'CHILD_THEME_URL', 'https://github.com/jjlazo79/woopersonalizador/' );
+define( 'CHILD_THEME_VERSION', '0.1' );
+
+/**
+ * HTML5 support
+ */
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+
+/**
+ * Fonts to mobile
+ */
+add_theme_support( 'genesis-responsive-viewport' );
+
+
+/**
+ * Proper way to enqueue scripts and styles.
+ */
+function customizer_bag_scripts() {
+    wp_enqueue_script( 'personalizador', get_stylesheet_directory_uri() . '/assets/js/personalizador.js', array(), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'customizer_bag_scripts' );
+
+
+/**
+ * Add template to
+ */
+function custom_single_product_template_include( $template ) {
+    if ( is_singular('product') && (has_term( 'personalizador', 'product_cat')) ) {
+        $template = get_stylesheet_directory() . '/woocommerce/single-product-personalizador.php';
+    }
+    return $template;
+}
+add_filter( 'template_include', 'custom_single_product_template_include', 50, 1 );
