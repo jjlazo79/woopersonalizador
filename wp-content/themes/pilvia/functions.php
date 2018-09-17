890088da79d7a87d8d754e5cfd1b5791f737f9d0
jose.lazo <jose.lazo@sngular.team>
2018-09-17 11:40:59 +0200

    Add template to functions

--- wp-content/themes/pilvia/functions.php
+++ wp-content/themes/pilvia/functions.php
@@ -30,3 +30,15 @@ function pilvia_scripts() {
     wp_enqueue_script( 'personalizador', get_stylesheet_directory_uri() . '/assets/js/personalizador.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'pilvia_scripts' );
+
+
+/**
+ * Add template to
+ */
+function custom_single_product_template_include( $template ) {
+    if ( is_singular('product') && (has_term( 'personalizador', 'product_cat')) ) {
+        $template = get_stylesheet_directory() . '/woocommerce/single-product-personalizador.php';
+    }
+    return $template;
+}
+add_filter( 'template_include', 'custom_single_product_template_include', 50, 1 );
